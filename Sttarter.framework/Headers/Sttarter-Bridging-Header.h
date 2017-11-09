aa160836331a46ba8face6629d13c1275d67de43
Muthu <muthu@Muthus-MacBook-Pro.local>
2017-11-09 12:32:18 +0530

    Added bridging header file to the public header

---
+++ Sttarter.framework/Headers/Sttarter-Bridging-Header.h
@@ -0,0 +1,64 @@
+//
+//  Sttarter-Bridging-Header.h
+//  Sttarter
+//
+//  Created by Muthu on 31/10/17.
+//  Copyright © 2017 Spurtree. All rights reserved.
+//
+
+#ifndef Sttarter_Bridging_Header_h
+#define Sttarter_Bridging_Header_h
+
+#import "DatabaseHandler.h"
+#import "DownloadManager.h"
+#import "DownloadManagerAdditions.h"
+#import "DownloadManagerConstants.h"
+#import "GetOtpModel.h"
+#import "OtpLoginModel.h"
+#import "BuzzMessagesModel.h"
+#import "GetRefCodeModel.h"
+#import "GetOtpModel.h"
+#import "OtpLoginModel.h"
+#import "MyTopicsModel.h"
+#import "BuzzMessagesModel.h"
+#import "AuthModel.h"
+#import "LoginModel.h"
+#import "SignUpModel.h"
+#import "GetRefCodeModel.h"
+#import "ReferrelSignupModel.h"
+#import "ChangeRefModel.h"
+#import "TrackUsageModel.h"
+#import "STTarterManager.h"
+#import "CouponManager.h"
+#import "CouponModel.h"
+#import "STTaterCommunicator.h"
+#import "TrackUsageModel.h"
+#import "UsersModel.h"
+#import "STTarterContentSystem.h"
+#import "STTarterReferrals.h"
+#import "Topics+CoreDataClass.h"
+#import "CreateWalletModel.h"
+#import "CreateWalletBaseModel.h"
+#import "WalletManager.h"
+#import "TopicMessage.h"
+#import "MQTT_QueueModel.h"
+#import "MessageStatusModel.h"
+#import "TopicMessage.h"
+#import "TopicMessageDetail.h"
+#import "transactedByModel.h"
+#import "TransactionsWalletModel.h"
+
+#import "JSONModelLib.h"
+#import "JSONModel.h"
+#import "AFNetworking.h"
+#import "MQTTClient.h"
+#import "MQTTSession.h"
+#import "MQTTSessionLegacy.h"
+#import "MQTTSessionSynchron.h"
+#import "MQTTMessage.h"
+#import "MQTTTransport.h"
+#import "MQTTCFSocketTransport.h"
+#import "MQTTCoreDataPersistence.h"
+#import "MQTTSSLSecurityPolicyTransport.h"
+
+#endif /* Sttarter_Bridging_Header_h */
